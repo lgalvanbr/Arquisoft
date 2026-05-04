7fe6bd053343681b57c9a174739a4882c644f3df
AriadnaVargas <123424168+AriadnaVargas@users.noreply.github.com>
2026-05-04 18:33:18 -0500

    a

--- finops_platform/settings.py
+++ finops_platform/settings.py
@@ -153,6 +153,11 @@ SOCIAL_AUTH_AUTH0_SCOPE = [
     'role',
 ]
 
+SOCIAL_AUTH_AUTH0_AUTH_EXTRA_ARGUMENTS = {
+    'audience': 'https://finops-api',
+    'prompt': 'login'  # ← fuerza pantalla de login siempre
+}
+
 
 # Usar pipeline por defecto de social_django (no personalizado)
 # El logout después de auth0 login se maneja con el parámetro ?just_logged_out=true en la URL
