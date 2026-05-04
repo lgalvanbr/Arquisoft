bff0ce6788184f4b70d17eac6b89c399c01a89e8
AriadnaVargas <123424168+AriadnaVargas@users.noreply.github.com>
2026-05-04 13:53:08 -0500

    merge conflicts

---
+++ autenticacion/migrations/0003_merge_0002_auditlog_0002_empresa.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('autenticacion', '0002_auditlog'),
+        ('autenticacion', '0002_empresa_intentoaccesonoautorizado_rechazointegridad_and_more'),
+    ]
+
+    operations = [
+    ]
